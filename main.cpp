14cf3de38465771d17e3a115d723eacc30d0c95e
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-05-16 11:46:08 -0700

    Finished adding functions that help to search and remove nodes

--- main.cpp
+++ main.cpp
@@ -23,12 +23,19 @@
  void add(Node*, Node*);
  void rearrange(Node*, Node* &);
  void print(Node*, int);
- void leftRotate(Node*);
- void rightRotate(Node*);
+ void leftRotate(Node* &, Node*);
+ void rightRotate(Node* &, Node*);
  void uncleFunc(Node*, Node* &);
  int pow(int, int);
- void charToIntArray(char* carray, int* &iarray, int &size);
+ void charToIntArray(char*, int* &, int &);
 
+ //Additional function prototypes below
+
+ void remove(Node* &, Node* &);
+ void doubleBlack(Node* &, Node*);
+ Node* getNode(Node*, int);
+ Node* leftestNode(Node*);
+ bool search(Node*, int);
 
  int main() {
 
@@ -81,26 +88,38 @@
        }
      
    }
-     else if (strcmp(name, "REMOVE") == 0) {
+     else if (strcmp(name, "REMOVE") == 0) { //Deletion part of RBT
        cout << "Please enter which number you want to be removed: ";
        cin >> value;
 
-       //insert search/remove functions
-
-       
-       
+       if(search(root, value) == true) { //looks if num is inside tree
+	 Node* temp = getNode(root, value); //traversed toward node
+	 cout << temp->getValue() << endl;
+	 remove(root, temp); //removes node
+       }
+       else {
+	 cout << "Invalid number!";
+       }
+       cout << endl;
+      
      }
      else if (strcmp(name, "PRINT") == 0) { //helps to print the picture of the tree!
 
      print(root, 0);
      
    }
-     else if (strcmp(name, "PRINT") == 0) {
+     else if (strcmp(name, "SEARCH") == 0) { //helps user to search for inputted number
 
        cout << "Please enter the number in which you want to search: ";
        cin >> value;
 
-       //search function here
+       if (search(root, value) == true) { //traverses tree to find node value which was inputted
+	 cout << "This number: " << value << " is existing within the tree.";
+       }
+       else {
+	 cout << "This number: " << value << " is not existing within the tree.";
+       }
+       cout << endl;
        
      }
      
@@ -123,7 +142,7 @@
 
  }
    
-void leftRotate(Node* blue) { //node blue
+void leftRotate(Node* &root, Node* blue) { //node blue
 
   if (blue->getRight() == NULL) { //yellow cease to occur
      return;
@@ -160,7 +179,7 @@ void leftRotate(Node* blue) { //node blue
 		  
  }
 
- void rightRotate(Node* yellow){
+ void rightRotate(Node* &root, Node* yellow){
 
    if (yellow->getLeft() == NULL){
     return;
@@ -290,20 +309,20 @@ void print(Node* current, int depth){ //visualization of tree
    }
  }
 
-void uncleFunc(Node* current, Node* &root){ //if uncle black
+ void uncleFunc(Node* current, Node* &root){ //if uncle black
 
    Node* parent = current->getParent();
    Node* grandparent = current->getGrandparent();
 
    if (current == parent->getRight() && parent == grandparent->getLeft()){
 
-     leftRotate(parent); //rotating the parent to other direction of grandparent
+     leftRotate(root, parent); //rotating the parent to other direction of grandparent
      current = current->getLeft(); //previous parent turns to current
 
    }
    else if (current == parent->getLeft() && parent == grandparent->getRight()){
 
-     rightRotate(parent);
+     rightRotate(root, parent);
      current = current->getRight();
 
    }
@@ -312,11 +331,11 @@ void uncleFunc(Node* current, Node* &root){ //if uncle black
    grandparent = current->getGrandparent();
 
    if (current == parent->getLeft()){ //grandp, parent, current creating line on left
-     rightRotate(grandparent); //rotating the grandparent
+     rightRotate(root, grandparent); //rotating the grandparent
    }
    
    else{ //the line is on the right
-     leftRotate(grandparent);
+     leftRotate(root, grandparent);
    }
    
    if (root == grandparent){ 
@@ -326,7 +345,7 @@ void uncleFunc(Node* current, Node* &root){ //if uncle black
    grandparent->setColor(2);
  }
  
-void charToIntArray(char* carray, int* &iarray, int &size){ //conversion of char array to int array
+ void charToIntArray(char* carray, int* &iarray, int &size){ //conversion of char array to int array
 
    int counter = 0;
    int place = 0;
@@ -361,3 +380,187 @@ void charToIntArray(char* carray, int* &iarray, int &size){ //conversion of char
    }
    return x;
  }
+
+ Node* getNode(Node* current, int value) { //this function helps to return node 
+
+   if(current == NULL) {
+     return current;
+   }
+   if (value == current->getValue()) { //if value found it stops looking
+     return current;
+   }
+   if (value < current->getValue()) { //left child is looked for if the value is smaller
+     return getNode(current->getLeft(), value);
+   }
+   if (value > current->getValue()) { //right child is looked for if value is larger
+     return getNode(current->getRight(), value);
+   }
+ }
+
+ Node* leftestNode(Node* current) { //helps to find the left most node within tree
+
+  if (current->getLeft() == NULL) { //if last node, return node
+     return current;
+   }
+   else {
+     return leftestNode(current->getLeft()); //traversal toward left child
+   }
+   
+ }
+
+ bool search(Node* current, int value) { //helps to search for node user inputted              
+
+   while (current != NULL) {
+     if (value == current->getValue()) { //if value within tree, true                            
+       return true;
+     }
+     if (value < current->getValue()) { //traversal                                              
+       return search(current->getLeft(), value);
+     }
+     if (value > current->getValue()) {
+       return search(current->getRight(), value);
+     }
+   }
+   return false; //if value not find, false                                                      
+ }
+
+ Node* replace(Node* node) { //helps to look for the replacement node
+
+   if (node->getLeft() != NULL && node->getRight() != NULL) { //goes toward the leftest of left!
+     return leftestNode(node->getRight());
+   }
+   if (node->getLeft() == NULL && node->getRight() == NULL) { //if there are no children
+     return NULL;
+   }
+   if (node->getLeft() != NULL) { //if there is a left child go left
+     return node->getLeft();
+   }
+   else { //go right
+     return node->getRight();
+   }
+ }
+
+ void remove(Node* &root, Node* &current) { //helps to remove node 
+
+   Node* temp = replace(current);
+   Node* parent = current->getParent();
+
+   if(temp == NULL) { //if the current is leaft
+     if (current == root) { //nodes root
+       root = NULL;
+     }
+     else {
+       if ((temp == NULL || temp->getColor() == 1) && (current->getColor() == 1)) { //if both black and the replacement is leaf
+	 doubleBlack(root, current);
+       }
+       else {
+	 if (current->getSibling() != NULL) {
+	   current->getSibling()->setColor(2); //sibling red
+	 }
+       }
+       if (parent->getRight() == current) { //reassigning
+	 parent->setRight(NULL);
+       }
+       else {
+	 parent->setLeft(NULL);
+       }
+     }
+     delete current;
+     return;
+   }
+   if (current->getLeft() == NULL || current->getRight() == NULL) { //if only one child
+     if (current == root) { //coying value then deleting if root
+       current->setValue(temp->getValue());
+       current->setLeft(NULL);
+       current->setRight(NULL);
+       delete temp;
+     }
+     else { //helping to move the temporary value up tree
+       if (parent->getLeft() == current) {
+	 parent->setLeft(temp);
+       }
+       else {
+	 parent->setRight(temp);
+       }
+       delete current;
+       temp->setParent(parent);
+       if ((temp == NULL || temp->getColor() == 1) && (current->getColor() == 1)) { //both black
+	 doubleBlack(root, temp);
+       }
+       else {
+	 temp->setColor(1);
+       }
+     }
+     return;
+   }
+   int temp2 = temp->getValue(); //if the temporary has two values helps to swap them
+   temp->setValue(current->getValue());
+   current->setValue(temp2);
+   remove(root, temp);
+   
+ }
+
+ void doubleBlack(Node* &root, Node* node) { //helps to sort with double black node
+
+   if (node == root) {
+     return;
+   }
+
+   Node* parent = node->getParent();
+   Node* sibling = node->getSibling();
+   if (sibling == NULL) { //if there is no sibling
+     doubleBlack(root, parent);
+   }
+   else {
+     if (sibling->getColor() == 2) { //if the sibling is red
+       parent->setColor(2);
+       sibling->setColor(1);
+       if (parent->getLeft() == sibling) { //if the sibling is on the left
+	 rightRotate(root, parent);
+       }
+       else { // if the sibling is on the right
+	 leftRotate(root, parent);
+       }
+       doubleBlack(root, node);
+     }
+     else { //if the sibling is black
+       if ((sibling->getLeft() != NULL && sibling->getLeft()->getColor() == 2) || (sibling->getRight() != NULL && sibling->getRight()->getColor() == 2)) {
+	 if ((sibling->getLeft() != NULL) && (sibling->getLeft()->getColor() == 2)) {
+	   if (parent->getLeft() == sibling) {
+	     sibling->getLeft()->setColor(sibling->getColor());
+	     sibling->setColor(parent->getColor());
+	     rightRotate(root, parent);
+	   }
+	   else { //if the sibling is on the right
+	     sibling->getLeft()->setColor(parent->getColor());
+	     rightRotate(root, sibling);
+	     leftRotate(root, parent);
+	   }
+	 }
+	 else { // if the rihgt child is red
+	   if(parent->getLeft() == sibling) {
+	     sibling->getRight()->setColor(parent->getColor());
+	     leftRotate(root, sibling);
+	     rightRotate(root, parent);
+	   }
+	   else { //if the sibling is on the rihgt
+	     sibling->getRight()->setColor(sibling->getColor());
+	     sibling->setColor(parent->getColor());
+	     leftRotate(root, parent);
+	   }
+	 }
+	 parent->setColor(1);
+       }
+       else { //both are black
+	 sibling->setColor(2);
+	 if (parent->getColor() == 1) {
+	   doubleBlack(root, parent);
+	 }
+	 else {
+	   parent->setColor(1);
+	 }
+       }
+     }
+   }
+ }
+			
